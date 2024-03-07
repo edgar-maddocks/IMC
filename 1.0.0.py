6992b53c554eae509c021e1a11eabd1662211497
edgar-maddocks <edgarmaddocks@outlook.com>
2024-03-07 14:16:42 +0000

    1.0.0

--- 1.0.0.py
+++ 1.0.0.py
@@ -4,6 +4,7 @@ import string
 
 import numpy as np
 import pandas as pd
+import statistics
 import jsonpickle
 
 
@@ -18,15 +19,16 @@ class Trader:
         for product in state.order_depths:
             products.append(product)
             product_order_depth: OrderDepth = state.order_depths[product]
-            orders: List[Order] = []
+            my_orders: List[Order] = []
 
+            available_orders[product] = {}
             if len(product_order_depth.buy_orders) != 0:
-                available_orders[product] = {"BID": product_order_depth.buy_orders}
+                available_orders[product]["BID"] = product_order_depth.buy_orders
 
             if len(product_order_depth.sell_orders) != 0:
-                available_orders[product] = {"ASK": product_order_depth.sell_orders}
+                available_orders[product]["ASK"] = product_order_depth.sell_orders
 
-            result[product] = orders
+            result[product] = my_orders
 
         if state.timestamp == 0:
             trader_data_DICT = {}
@@ -34,17 +36,26 @@ class Trader:
         elif state.timestamp != 0:
             trader_data_DICT = jsonpickle.loads(state.traderData)
             trader_data_DICT[state.timestamp] = available_orders
-            if state.timestamp > (200 * 100):
+            if state.timestamp > (20 * 100):
                 lowest_key = min([int(key) for key in trader_data_DICT.keys()])
                 del trader_data_DICT[str(lowest_key)]
 
         serialized_trader_data_DICT = jsonpickle.dumps(trader_data_DICT)
         traderData = serialized_trader_data_DICT
 
-        sma_spreads = {}
-
-        for dict in list(trader_data_DICT.values()):
-            print(dict)
+        spreads = {}
+        for product in products:
+            bids = np.array([])
+            asks = np.array([])
+            for dict in list(trader_data_DICT.values()):
+                bids = np.append(
+                    bids, np.mean([float(x) for x in list(dict[product]["BID"].keys())])
+                )
+                asks = np.append(
+                    asks, np.mean([float(x) for x in list(dict[product]["ASK"].keys())])
+                )
+            spread = asks - bids
+            spreads[product] = spread
 
         # Sample conversion request. Check more details below.
         conversions = 1
